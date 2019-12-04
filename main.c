df5e46cc08afc83b6545b5390a01aa240420a99f
JimWeimer <54601389+JimWeimer@users.noreply.github.com>
2019-12-04 13:34:17 -0600

    Update main.c

--- main.c
+++ main.c
@@ -6,5 +6,5 @@ int main(){
   printf("Type 'funny' or 'inspirational' to receive a funny or inspirational quote, respectively: \n");
   scanf("%s", userInput);
   
-  printf(userInput);
+  printf("%s", userInput);
 }
