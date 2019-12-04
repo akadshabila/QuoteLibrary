0fe0ab7e8a7614ae4ba96d5e5328978295e0ffec
JimWeimer <54601389+JimWeimer@users.noreply.github.com>
2019-12-04 13:42:48 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 
 int main(){
+  //reading user input to determine the type of quote to send out
   char* userInput;
   printf("Type 'funny' or 'inspirational' to receive a funny or inspirational quote, respectively: \n");
   scanf("%s", userInput);
   
-  printf("%s", userInput);
+  char* theQuote;
+  switch(userInput){
+    case 'funny' :
+      //fetch a funny quote and save it to theQuote
+      printf("funny");
+    case 'inspirational' :
+      //fetch an inspirational quote and save it to theQuote
+      printf("inspirational");
+  }
+  
+  //printf("%s", userInput);
 }
