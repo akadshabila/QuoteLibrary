2e0d0fbf278137da37a5718db6e96e38a2f643a3
JimWeimer <54601389+JimWeimer@users.noreply.github.com>
2019-12-04 13:54:12 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 
 int main(){
   //reading user input to determine the type of quote to send out
-  char* userInput;
-  printf("Type 'f' or 'i' to receive a funny or inspirational quote, respectively: \n");
+  char userInput = '';
+  printf("Type 'F' or 'I' to receive a funny or inspirational quote, respectively: \n");
   scanf("%c", userInput);
   
   char* theQuote;
   switch(userInput){
-    case 'f' :
+    case 'F' :
       //fetch a funny quote and save it to theQuote
-      printf("funny");
-    case 'i' :
+      printf("funny\n");
+      break;
+    case 'I' :
       //fetch an inspirational quote and save it to theQuote
-      printf("inspirational");
+      printf("inspirational\n");
+      break;
+    default :
+      printf("Invalid request\n");
   }
   
   //printf("%s", userInput);
