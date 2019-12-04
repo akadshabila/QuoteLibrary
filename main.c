51036b9c7a672dc17e5470f1ec7f45a45450e5b4
JimWeimer <54601389+JimWeimer@users.noreply.github.com>
2019-12-04 14:41:47 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* chooseQuote(char* filename){
+  char aQuote[100];
+  int r = rand() % 20; //random number between 0 and 19, there are 20 lines for each txt file
+  int currentLine = 0;
+  int i;
+  char c;
+  FILE *fp = fopen(filename, "r");
+  
+  //go through the text file for r lines, then save the rth line to aQuote
+  for(c = getc(fp); c != EOF; c = getc(fp)){
+    if(currentLine == r){
+      //if the current line is the line we want to read, save each letter to aQuote until newline is hit
+      if(c != '\n'){
+        aQuote[i] = c
+      }
+      else{
+        currentLine++;
+        break;
+      }
+    }
+    else if(c == '\n'){
+      currentLine++;
+    }
+  }
+  
+  return aQuote;
+}
+
 int main(){
   //reading user input to determine the type of quote to send out
   char userInput;
-  printf("Type 'F' or 'I' to receive a funny or inspirational quote, respectively: \n");
+  printf("Type 'F' or 'I' or 'G' to receive a funny or inspirational or George quote, respectively: \n");
   scanf("%c", &userInput);
   
   char* theQuote;
   switch(userInput){
     case 'F' :
       //fetch a funny quote and save it to theQuote
-      printf("funny\n");
+      theQuote = chooseQuote("funnyQuotes.txt");
       break;
     case 'I' :
       //fetch an inspirational quote and save it to theQuote
-      printf("inspirational\n");
+      theQuote = chooseQuote("inspirationalQuotes.txt");
+      break;
+    case 'G' :
+      //fetch an inspirational quote and save it to theQuote
+      theQuote = chooseQuote("georgeQuotes.txt");
       break;
     default :
       printf("Invalid request\n");
   }
   
-  //printf("%s", userInput);
+  //print the retreived quote
+  printf("%s", theQuote);
 }
